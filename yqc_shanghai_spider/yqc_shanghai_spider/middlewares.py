bc5f7db9d93a9bedfcc3a1c2a20dd79ddfa73802
james <james@JamesUbuntu>
2019-12-23 22:58:54 +0800

    update

--- yqc_shanghai_spider/yqc_shanghai_spider/middlewares.py
+++ yqc_shanghai_spider/yqc_shanghai_spider/middlewares.py
@@ -19,8 +19,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 class YqcShanghaiSpiderDownloaderMiddleware(object):
     def __init__(self):
-        # self.driver = webdriver.Chrome(executable_path=r"/home/james/_AllDocMap/06_Software/chromedriver")
-        self.driver = webdriver.Chrome(executable_path=r"/Users/qjiang/install/chromedriver")
+        self.driver = webdriver.Chrome(executable_path=r"/home/james/_AllDocMap/06_Software/chromedriver")
+        # self.driver = webdriver.Chrome(executable_path=r"/Users/qjiang/_AllDocMap/06_Software/install/chromedriver")
 
     def process_request(self, request, spider):
         print(">>> process_request(): " + str(request.url))
