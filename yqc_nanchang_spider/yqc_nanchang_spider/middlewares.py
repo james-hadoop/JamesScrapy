43d9eb41574b4eba8fb14a54e954c53938367456
james <james@JamesUbuntu>
2020-06-07 20:25:47 +0800

    add nanchang

--- yqc_nanchang_spider/yqc_nanchang_spider/middlewares.py
+++ yqc_nanchang_spider/yqc_nanchang_spider/middlewares.py
@@ -4,100 +4,105 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+from telnetlib import EC
 
 from scrapy import signals
+from selenium import webdriver
+import time
+import datetime
+from scrapy.http.response.html import HtmlResponse
+import traceback
+import logging
+import os
+
+import scrapy
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-class YqcNanchangSpiderSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+# class YqcNanchangSpiderSpiderMiddleware(object):
+#     # Not all methods need to be defined. If a method is not defined,
+#     # scrapy acts as if the spider middleware does not modify the
+#     # passed objects.
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
+#
+#     def process_spider_input(self, response, spider):
+#         # Called for each response that goes through the spider
+#         # middleware and into the spider.
+#
+#         # Should return None or raise an exception.
+#         return None
+#
+#     def process_spider_output(self, response, result, spider):
+#         # Called with the results returned from the Spider, after
+#         # it has processed the response.
+#
+#         # Must return an iterable of Request, dict or Item objects.
+#         for i in result:
+#             yield i
+#
+#     def process_spider_exception(self, response, exception, spider):
+#         # Called when a spider or process_spider_input() method
+#         # (from other spider middleware) raises an exception.
+#
+#         # Should return either None or an iterable of Request, dict
+#         # or Item objects.
+#         pass
+#
+#     def process_start_requests(self, start_requests, spider):
+#         # Called with the start requests of the spider, and works
+#         # similarly to the process_spider_output() method, except
+#         # that it doesn’t have a response associated.
+#
+#         # Must return only requests (not items).
+#         for r in start_requests:
+#             yield r
+#
+#     def spider_opened(self, spider):
+#         spider.logger.info('Spider opened: %s' % spider.name)
 
 
 class YqcNanchangSpiderDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
+    url_prefix = 'http://www.nc.gov.cn'
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+    def __init__(self):
+        os_info = os.uname()
+        if os_info.sysname == 'Darwin':
+            chrome_path = r"/Users/qjiang/install/chromedriver"
+        else:
+            chrome_path = r"/home/james/_AllDocMap/06_Software/chromedriver"
+        self.driver = webdriver.Chrome(executable_path=chrome_path)
 
     def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
+        url = request.url
+        print(
+            "1. process_request(): " + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + " -> " + url)
+        self.driver.get(url)
+        source = self.driver.page_source
 
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
+        if str('currentPage') not in url:
+            print("3. if finish process_request(): " + datetime.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S.%f') + " -> " + url)
+            # if str('REPORT_NDOC_006051') in url or str('REPORT_NDOC_006010') in url:
+            #     print(">>> debug: " + url)
+            #     print(source)
 
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
+            response = HtmlResponse(url=url, body=source, request=request, encoding="utf-8")
+            return response
 
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
+        else:
+            next_page = self.driver.find_element_by_xpath(
+                "//*[@id='4864']/table/tbody/tr/td/table/tbody/tr/td[8]/a")
+            url = str(next_page.find_element_by_xpath("./a").get_attribute('href'))
 
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
+            print("3. else finish process_request(): " + datetime.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S.%f') + " -> " + url)
 
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+            response = HtmlResponse(url=url, body=source, request=request, encoding="utf-8")
+            return response
